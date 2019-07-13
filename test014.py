e6026a89aaa0430d6f58711aed4bf611d72ca421
hellohangzhang <hellohangzhang@gmail.com>
2019-07-13 23:32:14 +0800

    Update test014.py

--- test014.py
+++ test014.py
@@ -2,9 +2,7 @@
 实例014：分解质因数
 题目 将一个整数分解质因数。例如：输入90,打印出90=2*3*3*5。
 '''
-
-
-prime_number_lest_than_100 = [2,3]
+prime_number = [2,3]
 n = 0 #counting the number of prime number
 k = 0 #counting the circulation number
 
@@ -16,18 +14,18 @@ for i in range(5,10001):
 			k = 0
 			break
 		if k == int(i**0.5) - 1:
-			prime_number_lest_than_100.append(i)
+			prime_number.append(i)
 			
 				
 number = int(input("Input a number:"))
 a = number
 zhiyinshu = []
-for i in range(len(prime_number_lest_than_100)):
+for i in range(len(prime_number)):
 	flag = True
 	while flag:
-		if number%prime_number_lest_than_100[i] == 0:
-			zhiyinshu.append(prime_number_lest_than_100[i])
-			number /= prime_number_lest_than_100[i]
+		if number%prime_number[i] == 0:
+			zhiyinshu.append(prime_number[i])
+			number /= prime_number[i]
 		else:
 			flag = False
 			
